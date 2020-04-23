060422115fe167a45ec750ffc06a3f83204ad7d3
kmega <charles.sz@protonmail.com>
2020-04-23 22:53:18 +0100

    Created repo for showcasing progress.

---
+++ DateRanges/DateRanges/DateTimeRange.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateRanges
+{
+    /// <summary>
+    /// Represents one continuos DateTime range
+    /// </summary>
+    public class DateTimeRange
+    {
+        /// <summary>
+        /// Range start
+        /// </summary>
+        public DateTime Start { get; set; }
+        /// <summary>
+        /// Range end
+        /// </summary>
+        public DateTime End { get; set; }
+
+        /// <summary>
+        /// Adds given range to current range
+        /// </summary>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public DateTimeRange[] Add(DateTimeRange range)
+        {
+
+        }
+
+        /// <summary>
+        /// Subtracts given range from current range
+        /// </summary>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public DateTimeRange[] Subtract(DateTimeRange range)
+        {
+
+        }
+
+        /// <summary>
+        /// Merges current range with given range
+        /// If ranges doesn't intersect, they are merged as longest period possible to create
+        /// </summary>
+        /// <param name="dateRange"></param>
+        public DateTimeRange Merge(DateTimeRange dateRange)
+        {
+            
+        }
+
+        /// <summary>
+        /// Expands current range to given date
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public DateTimeRange ExpandTo(DateTime date)
+        {
+
+        }
+
+        /// <summary>
+        /// Return value indicating if current range intersects with given range
+        /// </summary>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public Boolean IntersectsWith(DateTimeRange range)
+        {
+
+        }
+
+        /// <summary>
+        /// Return value indicating if current range contains whole given range
+        /// </summary>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public Boolean Contains(DateTimeRange range)
+        {
+
+        }
+
+        /// <summary>
+        /// Returns value indicating if current range starts with given range
+        /// </summary>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public Boolean StartsWith(DateTimeRange range)
+        {
+
+        }
+
+        /// <summary>
+        /// Returns value indicating if current range ends with given range
+        /// </summary>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public Boolean EndsWith(DateTimeRange range)
+        {
+
+        }
+        
+        /// <summary>
+        /// Gets value indicating if current range is continuation of given date time range
+        /// </summary>
+        /// <param name="dateRange"></param>
+        /// <returns></returns>
+        public Boolean IsContinuationOf(DateTimeRange dateRange)
+        {
+            
+        }
+
+        /// <summary>
+        /// Returns value indicating if current range is equal to given range
+        /// </summary>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public Boolean IsEqualTo(DateTimeRange range)
+        {
+
+        }
+
+        /// <summary>
+        /// Return value indicating if current range contains given date
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public Boolean Contains(DateTime date)
+        {
+            
+        }
+    }
+}
